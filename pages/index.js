7144ec7310e81b6ae94584d8b967403e4924e4c2
hanbrandon <hanbrandon@hotmail.com>
2021-12-05 19:24:20 -0800

    Navbar and Layout

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,32 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import SearchBar from '../components/SearchBar';
+import { useState, useEffect } from 'react';
+import Gallery from '../components/Gallery';
+import { Container, Row, Col } from 'react-bootstrap';
+import Layout from '../components/Layout';
 
-export default function Home() {
-	return <div></div>;
-}
+const Index = () => {
+	return (
+		<Layout>
+			<main>
+				<Container>
+					<Row>
+						<Col>
+							<SearchBar />
+						</Col>
+					</Row>
+				</Container>
+				<Container>
+					<Row>
+						<Col>
+							<Gallery />
+						</Col>
+					</Row>
+				</Container>
+			</main>
+		</Layout>
+	);
+};
+
+export default Index;
