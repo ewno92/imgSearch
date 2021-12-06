df972e988c6f56de26f2376ba92a0b7471ce401d
hanbrandon <hanbrandon@hotmail.com>
2021-12-05 20:21:23 -0800

    images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	publicRuntimeConfig: {
+		PIXABAY_API_KEY: process.env.PIXABAY_API_KEY,
+	},
+	reactStrictMode: true,
+	images: {
+		domains: ['cdn.pixabay.com'],
+	},
+};
