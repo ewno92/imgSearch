439084465aaf1f55c8584a81a766c5284f05f4c1
hanbrandon <hanbrandon@hotmail.com>
2021-12-08 00:28:31 -0800

    fecthing

--- pages/index.js
+++ pages/index.js
@@ -1,132 +1,112 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import axios from "axios";
-import Img from "../components/Img";
-import Masonry from "react-masonry-css";
-import InfiniteScroll from "react-infinite-scroller";
-import { Container, Row, Col } from "react-bootstrap";
-import Layout from "../components/Layout";
-import SearchBar from "../components/SearchBar";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import Img from '../components/Img';
+import Masonry from 'react-masonry-css';
+import InfiniteScroll from 'react-infinite-scroller';
+import { Container, Row, Col } from 'react-bootstrap';
+import Layout from '../components/Layout';
+import SearchBar from '../components/SearchBar';
 
 let pageNum = 1;
 
 const App = () => {
-  const router = useRouter();
-  const [imagesArray, setImagesArray] = useState([]);
-  const [totalPages, setTotalPages] = useState(0);
-  const [search, setSearch] = useState("");
-  const [select, setSelect] = useState("images");
-  const [imgs, setImgs] = useState({});
+	const router = useRouter();
+	const [imagesArray, setImagesArray] = useState([]);
+	const [totalPages, setTotalPages] = useState(0);
+	const [search, setSearch] = useState('');
+	const [select, setSelect] = useState('images');
+	const [imgs, setImgs] = useState({});
 
-  const PIXABAY_API_KEY = process.env.PIXABAY_API_KEY;
+	const PIXABAY_API_KEY = process.env.PIXABAY_API_KEY;
 
-  const API = axios.create({
-    baseURL: "https://pixabay.com/api/",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    params: {
-      key: PIXABAY_API_KEY,
-      safesearch: true,
-    },
-  });
+	const fetchImages = (pageNumber, keyword) => {
+		const URL = `https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=${keyword ? keyword : ''}&image_type=photo&per_page=12&page=${pageNumber}`;
+		console.log(URL);
+		axios
+			.get(URL)
+			.then((res) => {
+				console.log(res.data.hits);
+				setImagesArray([...imagesArray, ...res.data.hits]);
+				setTotalPages(res.data.totalHits / res.data.hits.length);
+			})
+			.catch((err) => console.log(err));
+	};
 
-  const fetchImages = (pageNumber) => {
-    API.get("/", { params: { page: pageNumber } })
-      .then((res) => {
-        console.log(res.data);
-        setImagesArray([...imagesArray, ...res.data.hits]);
-        setTotalPages(res.data.totalHits / res.data.hits.length);
-      })
-      .catch((err) => console.log(err));
-  };
+	// function to fetch images based on the
+	useEffect(() => {
+		fetchImages(pageNum);
+	}, []);
 
-  // function to fetch images based on the
-  useEffect(() => {
-    fetchImages(pageNum);
-  }, []);
+	const breakpointColumnsObj = {
+		default: 6,
+		1200: 3,
+		992: 3,
+		768: 2,
+		576: 1,
+	};
 
-  const breakpointColumnsObj = {
-    default: 6,
-    1200: 3,
-    992: 3,
-    768: 2,
-    576: 1,
-  };
+	const handleKeyword = (e) => {
+		setSearch(e.target.value);
+		console.log(search);
+	};
+	const handleSelect = (e) => {
+		setSelect(e.target.value);
+		console.log(search);
+	};
 
-  const handleKeyword = (e) => {
-    setSearch(e.target.value);
-    console.log(search);
-  };
-  const handleSelect = (e) => {
-    setSelect(e.target.value);
-    console.log(search);
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setImagesArray([], () => {
+			setTotalPages(0, () => {
+				setPageNum(0, () => {
+					fetchImages(1, search);
+				});
+			});
+		});
+		// setTotalPages(0);
+		// fetchImages(1, search);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetchImages(1);
-  };
-
-  return (
-    <Layout>
-      <button
-        onClick={() => {
-          fetchImages(1);
-        }}
-      >
-        click
-      </button>
-      <Container fluid>
-        <Row>
-          <Col>
-            <SearchBar
-              search={search}
-              select={select}
-              setSelect={setSelect}
-              handleKeyword={handleKeyword}
-              handleSelect={handleSelect}
-              handleSubmit={handleSubmit}
-            />
-          </Col>
-        </Row>
-      </Container>
-      <Container fluid>
-        <Row>
-          <Col md={12}>
-            <InfiniteScroll
-              pageStart={0}
-              loadMore={() => fetchImages(++pageNum)}
-              hasMore={pageNum < totalPages ? true : false}
-            >
-              <Masonry
-                breakpointCols={breakpointColumnsObj}
-                className="masonry-grid"
-                columnClassName="masonry-grid_column"
-              >
-                {imagesArray.map((image) => (
-                  <div
-                    className="img-container px-3"
-                    style={{ cursor: "pointer" }}
-                    onClick={() => {
-                      router.push(`/photos/${image.id}`);
-                    }}
-                  >
-                    <Img
-                      sylte={{ cursor: "pointer" }}
-                      key={image.id}
-                      {...image}
-                    />
-                  </div>
-                ))}
-              </Masonry>
-            </InfiniteScroll>
-          </Col>
-        </Row>
-      </Container>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Container fluid>
+				<Row>
+					<Col>
+						<SearchBar
+							search={search}
+							select={select}
+							setSelect={setSelect}
+							handleKeyword={handleKeyword}
+							handleSelect={handleSelect}
+							handleSubmit={handleSubmit}
+						/>
+					</Col>
+				</Row>
+			</Container>
+			<Container fluid>
+				<Row>
+					<Col md={12}>
+						<InfiniteScroll pageStart={0} loadMore={() => fetchImages(++pageNum, search)} hasMore={pageNum < totalPages ? true : false}>
+							<Masonry breakpointCols={breakpointColumnsObj} className="masonry-grid" columnClassName="masonry-grid_column">
+								{imagesArray.map((image) => (
+									<div
+										className="img-container px-3"
+										style={{ cursor: 'pointer' }}
+										onClick={() => {
+											router.push(`/photos/${image.id}`);
+										}}
+									>
+										<Img sylte={{ cursor: 'pointer' }} key={image.id} {...image} />
+									</div>
+								))}
+							</Masonry>
+						</InfiniteScroll>
+					</Col>
+				</Row>
+			</Container>
+		</Layout>
+	);
 };
 
 export default App;
